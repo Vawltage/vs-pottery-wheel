9b260946b3f9c1d968ad34a39fbab34cd53fee8e
Vawltage <qegabe@gmail.com>
2023-01-09 07:43:15 -0500

    Initial Commit

---
+++ src/claywheel.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent.Mechanics;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Client;
+using Vintagestory.API.Util;
+using Vintagestory.API.Datastructures;
+
+namespace claywheel.src
+{
+    class ClayWheel : BlockMPBase
+    {
+        WorldInteraction[] interactions;
+
+        public override void OnLoaded(ICoreAPI api)
+        {
+            base.OnLoaded(api);
+
+            if (api.Side != EnumAppSide.Client) return;
+            ICoreClientAPI capi = api as ICoreClientAPI;
+
+            interactions = ObjectCacheUtil.GetOrCreate(api, "clayWheelInteractions", () =>
+            {
+                List<ItemStack> clayStackList = new List<ItemStack>();
+
+                foreach (CollectibleObject obj in api.World.Collectibles)
+                {
+                    string firstCodePart = obj.FirstCodePart();
+
+                    if (firstCodePart == "clay")
+                    {
+                        List<ItemStack> stacks = obj.GetHandBookStacks(capi);
+                        if (stacks != null) clayStackList.AddRange(stacks);
+                    }
+                }
+
+                return new WorldInteraction[]
+                {
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "claywheel:blockhelp-claywheel-startclay",
+                        HotKeyCode = "shift",
+                        MouseButton = EnumMouseButton.Right,
+                        Itemstacks = clayStackList.ToArray(),
+                        GetMatchingStacks = (wi, bs, es) =>
+                        {
+                            ClayWheelEntity becw = api.World.BlockAccessor.GetBlockEntity(bs.Position) as ClayWheelEntity;
+                            if (becw != null && becw.SelectedRecipe != null)
+                            {
+                                return null;
+                            }
+                            else return clayStackList.ToArray();
+                        }
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "claywheel:blockhelp-claywheel-addclay",
+                        HotKeyCode = null,
+                        MouseButton = EnumMouseButton.Right,
+                        Itemstacks = clayStackList.ToArray(),
+                        GetMatchingStacks = (wi, bs, es) =>
+                        {
+                            ClayWheelEntity becw = api.World.BlockAccessor.GetBlockEntity(bs.Position) as ClayWheelEntity;
+                            List<ItemStack> stacks = new List<ItemStack>();
+
+                            foreach (var val in wi.Itemstacks)
+                            {
+                                if (becw != null)
+                                {
+                                    if (becw.WorkItemStack != null && becw.WorkItemStack.Collectible.LastCodePart() == val.Collectible.LastCodePart())
+                                    {
+                                        stacks.Add(val);
+                                    }  
+                                }
+                            }
+                            return stacks.ToArray();
+                        }
+                    },
+                    new WorldInteraction()
+                    {
+                        ActionLangCode = "claywheel:blockhelp-claywheel-retrieveclay",
+                        HotKeyCode = "shift",
+                        MouseButton = EnumMouseButton.Right,
+                        Itemstacks = clayStackList.ToArray(),
+                        RequireFreeHand = true,
+                        GetMatchingStacks = (wi, bs, es) =>
+                        {
+                            ClayWheelEntity becw = api.World.BlockAccessor.GetBlockEntity(bs.Position) as ClayWheelEntity;
+                            if (becw != null && becw.SelectedRecipe != null)
+                            {
+                                return new ItemStack[] { becw.WorkItemStack };
+                            }
+                            else return null;
+                        }
+                    }
+                };
+            }
+            );
+            
+        }
+
+        public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
+        {
+            bool ok = base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref failureCode);
+
+            if (ok)
+            {
+                bool poop = tryConnect(world, byPlayer, blockSel.Position, BlockFacing.DOWN);
+            }
+
+            return ok;
+        }
+
+        //order of methods is: Start > Step(as long as being held) > Cancel > Stop
+        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+            var be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as ClayWheelEntity;
+            if (be != null)
+            {
+                ItemSlot slot = byPlayer.InventoryManager.ActiveHotbarSlot;
+                if (slot.Itemstack == null)
+                {
+                    if (be.WorkItemStack != null && byPlayer.Entity.Controls.ShiftKey)
+                    {
+                        //If empty hand and shift+rmb return clay
+                        be.RetreiveClay(byPlayer);
+                        return true;
+                    }
+                    else return false;
+                } 
+                string clayOnWheelType = be.WorkItemStack != null ? be.WorkItemStack.Collectible.Code.ToString() : null;
+                CollectibleObject heldItem = slot.Itemstack.Collectible;
+                if (byPlayer.Entity.Controls.ShiftKey && be.SelectedRecipe == null && heldItem.Code.FirstCodePart() == "clay")
+                {
+                    //if shift+rmb and clay in hand and no current recipe pull up recipe selector and don't spin
+                    be.PutClay(slot, byPlayer);
+                    return true;
+                }
+                else if (be.SelectedRecipe != null && heldItem.Code.ToString() == clayOnWheelType)
+                {
+                    //if rmb with correct clay only spin on start and add on step
+                    be.Spin(true);
+                    return true;
+                }
+                else return false;
+            }
+            return base.OnBlockInteractStart(world, byPlayer, blockSel);
+        }
+
+        public override bool OnBlockInteractStep(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+            var be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as ClayWheelEntity;
+            if (be != null)
+            {
+                ItemSlot slot = byPlayer.InventoryManager.ActiveHotbarSlot;
+                if (slot.Itemstack == null) return false;
+                string clayOnWheelType = be.WorkItemStack != null ? be.WorkItemStack.Collectible.Code.ToString() : null;
+                CollectibleObject heldItem = slot.Itemstack.Collectible;
+                if (be.SelectedRecipe != null && heldItem.Code.ToString() == clayOnWheelType)
+                {
+                    //add clay and spin when holding clay while holding rmb
+                    be.PutClay(slot, byPlayer);
+                    be.Spin(true);
+                    return true;
+                }
+                else return false;
+            }
+            return base.OnBlockInteractStep(secondsUsed, world, byPlayer, blockSel);
+        }
+
+        public override bool OnBlockInteractCancel(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel, EnumItemUseCancelReason cancelReason)
+        {
+            var be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as ClayWheelEntity;
+            if (be != null)
+            {
+                be.Spin(false);
+                return true;
+            }
+            return base.OnBlockInteractCancel(secondsUsed, world, byPlayer, blockSel, cancelReason);
+        }
+
+        public override void OnBlockInteractStop(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+            var be = world.BlockAccessor.GetBlockEntity(blockSel.Position) as ClayWheelEntity;
+            if (be != null)
+            {
+                be.Spin(false);
+            }
+        }
+
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
+        {
+            return interactions.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
+        }
+
+        public override void DidConnectAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
+        {
+
+        }
+
+        public override bool HasMechPowerConnectorAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
+        {
+            return face == BlockFacing.DOWN;
+        }
+    }
+}
---
+++ src/claywheelrenderer.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent.Mechanics;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Client;
+using Vintagestory.API.Util;
+using Vintagestory.API.Datastructures;
+
+namespace claywheel.src
+{
+    public class ClayWheelRenderer : IRenderer
+    {
+        internal bool ShouldRender;
+        internal bool ShouldRotateManual;
+        internal bool ShouldRotateAutomated;
+
+        public BEBehaviorMPConsumer mechPowerPart;
+
+        private ICoreClientAPI api;
+        private BlockPos pos;
+        private const float spinSpeed = 120;
+
+        MeshRef baseMeshRef;
+        public Matrixf BaseModelMat = new Matrixf();
+
+        int texId;
+        MeshRef workItemMeshRef;
+        public Matrixf WorkItemModelMat = new Matrixf();
+
+        public float AngleRad;
+
+        public ClayWheelRenderer(ICoreClientAPI coreClientAPI, BlockPos pos, MeshData mesh)
+        {
+            this.api = coreClientAPI;
+            this.pos = pos;
+            baseMeshRef = coreClientAPI.Render.UploadMesh(mesh);
+        }
+
+        public double RenderOrder
+        {
+            get { return 0.5; }
+        }
+
+        public int RenderRange
+        {
+            get { return 24; }
+        }
+
+        public void OnRenderFrame(float deltaTime, EnumRenderStage stage)
+        {
+            if (workItemMeshRef != null)
+            {
+                RenderWorkItem();
+            }
+
+            //if the wheel is not spinning or the base mesh is null return
+            if (baseMeshRef == null || !ShouldRender) return;
+
+            IRenderAPI rpi = api.Render;
+            Vec3d camPos = api.World.Player.Entity.CameraPos;
+
+            rpi.GlDisableCullFace();
+            rpi.GlToggleBlend(true);
+
+            IStandardShaderProgram prog = rpi.PreparedStandardShader(pos.X, pos.Y, pos.Z);
+            prog.Tex2D = api.BlockTextureAtlas.AtlasTextureIds[0];
+
+
+            prog.ModelMatrix = BaseModelMat
+                .Identity()
+                .Translate(pos.X - camPos.X, pos.Y - camPos.Y, pos.Z - camPos.Z)
+                //center the model before rotating
+                .Translate(0.5f, 0, 0.5f)
+                .RotateY(AngleRad)
+                .Translate(-0.5f, 0, -0.5f)
+                .Values;
+
+            prog.ViewMatrix = rpi.CameraMatrixOriginf;
+            prog.ProjectionMatrix = rpi.CurrentProjectionMatrix;
+            rpi.RenderMesh(baseMeshRef);
+            prog.Stop();
+
+
+            if (ShouldRotateManual)
+            {
+                AngleRad += deltaTime * spinSpeed * GameMath.DEG2RAD;
+            }
+
+            if (ShouldRotateAutomated && mechPowerPart != null)
+            {
+                AngleRad = mechPowerPart.AngleRad;
+            }
+        }
+
+        private void RenderWorkItem()
+        {
+            IRenderAPI rpi = api.Render;
+            IClientWorldAccessor worldAccess = api.World;
+            Vec3d camPos = worldAccess.Player.Entity.CameraPos;
+
+            rpi.GlDisableCullFace();
+            IStandardShaderProgram prog = rpi.PreparedStandardShader(pos.X, pos.Y, pos.Z);
+            rpi.BindTexture2d(texId);
+
+            prog.ModelMatrix = WorkItemModelMat
+                .Identity()
+                .Translate(pos.X - camPos.X, pos.Y - camPos.Y, pos.Z - camPos.Z)
+                .Translate(0.5f, 1f, 0.5f)
+                .RotateY(AngleRad)
+                .Translate(-0.5f, 0, -0.5f)
+                .Values;
+
+            prog.ViewMatrix = rpi.CameraMatrixOriginf;
+            prog.ProjectionMatrix = rpi.CurrentProjectionMatrix;
+
+            rpi.RenderMesh(workItemMeshRef);
+
+            prog.Stop();
+        }
+
+        public void RegenMesh(ItemStack workitem, bool[,,] Voxels)
+        {
+            if (workItemMeshRef != null)
+            {
+                workItemMeshRef.Dispose();
+            }
+            workItemMeshRef = null;
+
+            if (workitem == null) return;
+
+            //this.workItem = workitem;
+            MeshData workItemMesh = new MeshData(24, 36, false);
+
+            float subPixelPaddingx = api.BlockTextureAtlas.SubPixelPaddingX;
+            float subPixelPaddingy = api.BlockTextureAtlas.SubPixelPaddingY;
+
+            string texname = api.World.GetItem(new AssetLocation("clayworkitem-" + workitem.Collectible.LastCodePart())).Code.ToShortString();
+            TextureAtlasPosition tpos = api.BlockTextureAtlas.GetPosition(api.World.GetBlock(new AssetLocation("clayform")), texname);
+            MeshData singleVoxelMesh = CubeMeshUtil.GetCubeOnlyScaleXyz(1 / 32f, 1 / 32f, new Vec3f(1 / 32f, 1 / 32f, 1 / 32f));
+            singleVoxelMesh.Rgba = new byte[6 * 4 * 4].Fill((byte)255);
+            CubeMeshUtil.SetXyzFacesAndPacketNormals(singleVoxelMesh);
+
+            texId = tpos.atlasTextureId;
+
+            for (int i = 0; i < singleVoxelMesh.Uv.Length; i++)
+            {
+                if (i % 2 > 0)
+                {
+                    singleVoxelMesh.Uv[i] = tpos.y1 + singleVoxelMesh.Uv[i] * 2f / api.BlockTextureAtlas.Size.Height - subPixelPaddingy;
+                }
+                else
+                {
+                    singleVoxelMesh.Uv[i] = tpos.x1 + singleVoxelMesh.Uv[i] * 2f / api.BlockTextureAtlas.Size.Width - subPixelPaddingx;
+                }
+            }
+
+            singleVoxelMesh.XyzFaces = (byte[])CubeMeshUtil.CubeFaceIndices.Clone();
+            singleVoxelMesh.XyzFacesCount = 6;
+
+            singleVoxelMesh.SeasonColorMapIds = new byte[6];
+            singleVoxelMesh.ClimateColorMapIds = new byte[6];
+            singleVoxelMesh.ColorMapIdsCount = 6;
+
+
+            MeshData voxelMeshOffset = singleVoxelMesh.Clone();
+
+            for (int x = 0; x < 16; x++)
+            {
+                for (int y = 0; y < 16; y++)
+                {
+                    for (int z = 0; z < 16; z++)
+                    {
+                        if (!Voxels[x, y, z]) continue;
+
+                        float px = x / 16f;
+                        float py = y / 16f;
+                        float pz = z / 16f;
+
+                        for (int i = 0; i < singleVoxelMesh.xyz.Length; i += 3)
+                        {
+                            voxelMeshOffset.xyz[i] = px + singleVoxelMesh.xyz[i];
+                            voxelMeshOffset.xyz[i + 1] = py + singleVoxelMesh.xyz[i + 1];
+                            voxelMeshOffset.xyz[i + 2] = pz + singleVoxelMesh.xyz[i + 2];
+                        }
+
+                        float offsetX = ((((x + 4 * y) % 16f / 16f)) * 32f) / api.BlockTextureAtlas.Size.Width;
+                        float offsetY = (pz * 32f) / api.BlockTextureAtlas.Size.Height;
+
+                        for (int i = 0; i < singleVoxelMesh.Uv.Length; i += 2)
+                        {
+                            voxelMeshOffset.Uv[i] = singleVoxelMesh.Uv[i] + offsetX;
+                            voxelMeshOffset.Uv[i + 1] = singleVoxelMesh.Uv[i + 1] + offsetY;
+                        }
+
+                        workItemMesh.AddMeshData(voxelMeshOffset);
+                    }
+                }
+            }
+
+            workItemMeshRef = api.Render.UploadMesh(workItemMesh);
+        }
+
+
+        public void Dispose()
+        {
+            api.Event.UnregisterRenderer(this, EnumRenderStage.Opaque);
+
+            if (baseMeshRef != null) baseMeshRef.Dispose();
+            if (workItemMeshRef != null) workItemMeshRef.Dispose();
+        }
+    }
+}
