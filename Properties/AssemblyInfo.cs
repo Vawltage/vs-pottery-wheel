c94badb045efd9e909ba68055591b4debd110458
Vawltage <geqcreature@gmail.com>
2023-04-22 21:32:40 -0400

    added AssemblyInfo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using Vintagestory.API.Common;
+
+[assembly: ModDependency("game", "1.18.0")]
+[assembly: ModInfo("ClayWheels", "claywheel", Version = "1.0.1", Authors = new string[] { "Voltage" }, Description = "Adds a pottery wheel for easier clay forming.")]
