f85591428af99f03a2d57ecc67fb16914a41e8fd
Vawltage <geqcreature@gmail.com>
2024-12-07 09:37:19 -0500

    update to 1.20.0-rc.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using Vintagestory.API.Common;
 
-[assembly: ModDependency("game", "1.20.0-pre.13")]
+[assembly: ModDependency("game", "1.20.0-rc.1")]
 [assembly: ModInfo("ClayWheels", "claywheel", Version = "1.1.2", Authors = new string[] { "Voltage" }, Description = "Adds a pottery wheel for easier clay forming.")]
