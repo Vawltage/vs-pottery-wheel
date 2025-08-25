959cbab5d6518c0d27dc172a83c111d3dc8d6b8a
Vawltage <geqcreature@gmail.com>
2025-08-25 19:05:04 -0400

    bump vs version to 1.21

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using Vintagestory.API.Common;
 
-[assembly: ModDependency("game", "1.20.0-rc.1")]
-[assembly: ModInfo("ClayWheels", "claywheel", Version = "1.1.2", Authors = new string[] { "Voltage" }, Description = "Adds a pottery wheel for easier clay forming.")]
+[assembly: ModDependency("game", "1.21.0")]
+[assembly: ModInfo("ClayWheels", "claywheel", Version = "1.1.3", Authors = new string[] { "Voltage" }, Description = "Adds a pottery wheel for easier clay forming.")]
