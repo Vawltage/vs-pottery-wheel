acf66c07870966dba2685e13c2e53a4f147070bc
Vawltage <geqcreature@gmail.com>
2025-10-21 15:10:01 -0400

    fix server config not synced with clients

--- PotteryWheel/ClayWheelModConfig.cs
+++ PotteryWheel/ClayWheelModConfig.cs
@@ -1,7 +1,10 @@
+using ProtoBuf;
+
 namespace SimplePotteryWheel;
 
+[ProtoContract]
 public class ClayWheelModConfig
 {
-    public int voxelsPerUse = 1;
-    public float poweredMultiplier = 2.0f;
+    [ProtoMember(1)] public int voxelsPerUse = 1;
+    [ProtoMember(2)] public float poweredMultiplier = 2.0f;
 }
--- PotteryWheel/ClayWheelModSystem.cs
+++ PotteryWheel/ClayWheelModSystem.cs
@@ -1,10 +1,14 @@
-﻿using Vintagestory.API.Common;
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 
 namespace SimplePotteryWheel;
 
 public class ClayWheelModSystem : ModSystem
 {
     public static ClayWheelModConfig config;
+    private INetworkChannel channel;
+
     public override void Start(ICoreAPI api)
     {
         base.Start(api);
@@ -17,7 +21,7 @@ public class ClayWheelModSystem : ModSystem
             if (config == null)
             {
                 config = new ClayWheelModConfig();
-                api.StoreModConfig<ClayWheelModConfig>(config, "potterywheel.json");
+                api.StoreModConfig(config, "potterywheel.json");
             }
         }
         catch (System.Exception)
@@ -27,5 +31,36 @@ public class ClayWheelModSystem : ModSystem
         }
 
     }
-}
 
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+        api.Event.PlayerNowPlaying += OnPlayerNowPlaying;
+
+        channel = api.Network.RegisterChannel("simplepotterywheel");
+        channel.RegisterMessageType<ClayWheelModConfig>();
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
+        channel = api.Network.RegisterChannel("simplepotterywheel");
+        channel.RegisterMessageType<ClayWheelModConfig>();
+        (channel as IClientNetworkChannel).SetMessageHandler<ClayWheelModConfig>(OnConfigMessage);
+    }
+
+
+    private void OnPlayerNowPlaying(IServerPlayer byPlayer)
+    {
+        if (channel is IServerNetworkChannel schannel)
+        {
+            schannel.SendPacket(config, [byPlayer]);
+        }
+    }
+
+    private void OnConfigMessage(ClayWheelModConfig modConfig)
+    {
+        config = modConfig;
+    }
+
+}
